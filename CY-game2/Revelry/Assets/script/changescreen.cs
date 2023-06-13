fa68ff76482d8da2246f9c324b1632b4921bc45c
Pigeon-hcy <129189900+Pigeon-hcy@users.noreply.github.com>
2023-06-13 14:29:30 +0800

    make a new scene

---
+++ CY-game2/Revelry/Assets/script/changescreen.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class changescreen : MonoBehaviour
+{
+
+    public string target = "SampleScene";
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Debug.Log("enter");
+            SceneManager.LoadScene(target);
+        }
+    }
+}
