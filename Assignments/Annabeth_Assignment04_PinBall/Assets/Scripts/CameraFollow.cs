76ee06abd93baa0241747b1e214ef8222d84b0fd
bellinimoon <mg6839@nyu.edu>
2023-07-11 13:22:04 -0400

    update

--- Assignments/Annabeth_Assignment04_PinBall/Assets/Scripts/CameraFollow.cs
+++ Assignments/Annabeth_Assignment04_PinBall/Assets/Scripts/CameraFollow.cs
@@ -6,20 +6,41 @@ public class CameraFollow : MonoBehaviour
 {
     public Transform target;
     public Vector3 offset;
+    private Vector3 _pos;
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform.position = new Vector3(0, 0, offset.z);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(target.position.y + offset.y <= 5.4 && target.position.y + offset.y >= 0)
+        //if ((target.position.x + offset.x <= 1 && target.position.x + offset.x >= -1) && (target.position.y + offset.y <= 5.4 && target.position.y + offset.y >= 0))
+        //{
+        //    _pos = new Vector3(target.position.x + offset.x, target.position.y + offset.y, offset.z);
+
+            //} else if (target.position.x + offset.x <= 1 && target.position.x + offset.x >= -1)
+            //{
+            //    _pos = new Vector3(target.position.x + offset.x, transform.position.y, offset.z);
+            //}
+            //else if (target.position.y + offset.y <= 5.4 && target.position.y + offset.y >= 0)
+            //{
+            //    //add y
+            //    _pos = new Vector3(transform.position.x, target.position.y + offset.y, offset.z);
+
+            //}
+
+        if (target.position.y + offset.y <= 4.5 && target.position.y + offset.y >= 0)
         {
-            transform.position = new Vector3(0, target.position.y + offset.y, offset.z);
+            //add y
+            _pos = new Vector3(0, target.position.y + offset.y, offset.z);
+            transform.position = _pos;
+
         }
-            //transform.position = new Vector3(target.position.x + offset.x, target.position.y + offset.y, offset.z);
-            
-    }
+        //transform.position = new Vector3(target.position.x + offset.x, target.position.y + offset.y, offset.z);
+        
+
+
+    } 
 }
